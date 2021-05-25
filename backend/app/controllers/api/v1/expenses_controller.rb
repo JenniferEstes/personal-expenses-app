e114bd2863cbbd0c3ace0624ddb221254c0f1b6c
Jennifer <jenndestes@gmail.com>
2021-05-24 22:50:17 -0500

    Commented scaffold made methods

--- backend/app/controllers/api/v1/expenses_controller.rb
+++ backend/app/controllers/api/v1/expenses_controller.rb
@@ -13,43 +13,43 @@ class Api::V1::ExpensesController < ApplicationController
   end
 
   # GET /expenses/1
-  def show
-    render json: @expense
-  end
+  # def show
+  #   render json: @expense
+  # end
 
   # POST /expenses
-  def create
-    @expense = Expense.new(expense_params)
-
-    if @expense.save
-      render json: @expense, status: :created, location: @expense
-    else
-      render json: @expense.errors, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @expense = Expense.new(expense_params)
+  #
+  #   if @expense.save
+  #     render json: @expense, status: :created, location: @expense
+  #   else
+  #     render json: @expense.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # PATCH/PUT /expenses/1
-  def update
-    if @expense.update(expense_params)
-      render json: @expense
-    else
-      render json: @expense.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @expense.update(expense_params)
+  #     render json: @expense
+  #   else
+  #     render json: @expense.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /expenses/1
-  def destroy
-    @expense.destroy
-  end
+  # def destroy
+  #   @expense.destroy
+  # end
 
-  private
+  # private
     # Use callbacks to share common setup or constraints between actions.
-    def set_expense
-      @expense = Expense.find(params[:id])
-    end
+    # def set_expense
+    #   @expense = Expense.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
-    def expense_params
-      params.require(:expense).permit(:date, :description, :amount)
-    end
+    # def expense_params
+    #   params.require(:expense).permit(:date, :description, :amount)
+    # end
 end
