5554052a6802c035569cd96b8fb43090c8cfe5ee
Jennifer <jenndestes@gmail.com>
2021-05-25 01:00:11 -0500

    Added create method to sessions controller, using json as the communication format

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -5,6 +5,15 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create
+    user = User.find_by(:email => params[:email])
+    if user && user.authenticate(params[:password])
+      render :json => user, :only => [:id, :email]
+    else
+      render :json => { errors: "Your login information does not match our records. Please try again." }
+    end
+  end
+
   def destroy
     session.clear
   end
