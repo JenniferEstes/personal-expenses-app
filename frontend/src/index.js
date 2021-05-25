7e7092b657286b267f3abbb84d20412e8a3d296f
Jennifer <jenndestes@gmail.com>
2021-05-24 23:42:04 -0500

    Displayed all of the user's expense data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,17 @@ document.addEventListener("DOMContentLoaded", () =>  {
 })
 
 function fetchExpenses() {
+    const expensesContainer = document.getElementById("expenses-container")
     // returns promise
     fetch("http://localhost:3000/api/v1/expenses")
         // Async too, returns promise
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(data => {
+            data.forEach(expense => {
+                expensesContainer.innerHTML += `<li>${expense.date}</li>`
+                expensesContainer.innerHTML += `<li>${expense.description}</li>`
+                expensesContainer.innerHTML += `<li>${expense.amount}</li>`
+            })
+        })
     // .catch(err => console.warn(err))
 }
