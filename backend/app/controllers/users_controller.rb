7542ec43997fea751ccd7eb4aa9136c0e65390ac
Jennifer <jenndestes@gmail.com>
2021-05-25 01:28:43 -0500

    Added show method and edited strong params

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,50 +1,36 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :update, :destroy]
-  # #
-  # # # GET /users
-  # def index
-  #   @users = User.all
-  #
-  #   render json: @users
-  # end
-  #
-  # # GET /users/1
-  # def show
-  #   render json: @user
-  # end
-  #
+  before_action :find_user, only: [:show]
+
+  # GET /users/1
+  def show
+    find_user
+    render json: @user
+  end
+
   # POST /users
   def create
-    user = User.new(user_params)
-    user = User.find_by(:email => params[:email])
-    if user.save
-      render json: user, status: :created, location: uuser
+    @user = User.new(user_params)
+    find_user
+    if @user.save
+      render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
-  # # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-  #
   # # DELETE /users/1
   # def destroy
   #   @user.destroy
   # end
-  #
+
   # private
   #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_user
-  #     @user = User.find(params[:id])
-  #   end
+    def find_user
+      @user = User.find_by_id(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:email, :password_digest)
     end
+
 end
