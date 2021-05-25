cbfca21a7daca637284194ab4470f9c5e0b500af
Jennifer <jenndestes@gmail.com>
2021-05-24 22:51:27 -0500

    Put first fetch in a function

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
-// returns promise
-fetch("http://localhost:3000/api/v1/expenses")
-    // Async too, returns promise
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-    .catch(err => console.warn(err))
+document.addEventListener("DOMContentLoaded", () =>  {
+    fetchExpenses()
+})
+
+function fetchExpenses() {
+    // returns promise
+    fetch("http://localhost:3000/api/v1/expenses")
+        // Async too, returns promise
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    // .catch(err => console.warn(err))
+}
