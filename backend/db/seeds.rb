a177b98d4113915f7600c03c861f078227c5e864
Jennifer <jenndestes@gmail.com>
2021-05-24 20:22:42 -0500

    Added seed data and user_id to Expenses table

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_is_jenny = User.create(email: "jenndestes@gmail.com")
+user_is_william = User.create(email: "billywilly@gmail.com")
+user_is_penelope = User.create(email: "cutedoggo@gmail.com")
+
+user_is_jenny.expenses.build(
+  date: "10/10/2020",
+  description: "birthday party",
+  amount: 200
+)
+
+user_is_william.expenses.build(
+  date: "4/20/2021",
+  description: "pineapple juice",
+  amount: 7,
+  )
+
+user_is_penelope.expenses.build(
+  date: "06/01/2020",
+  description: "dog treats",
+  amount: 10,
+  )
+
+user_is_jenny.save
+user_is_william.save
+user_is_penelope.save
