c74256e3f72a307a8538e57a67368adf109bfd3e
Jennifer <jenndestes@gmail.com>
2021-05-24 19:02:32 -0500

    Nested user resources under V1, as well as, V1 under API

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,8 @@
  Rails.application.routes.draw do
   # resources :expenses
-  resources :users, only: [:index]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index]
+    end
+  end
 end
