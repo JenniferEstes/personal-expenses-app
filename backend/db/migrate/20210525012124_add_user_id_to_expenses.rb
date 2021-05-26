703495c5d9de7255b0a5bbdac8d90b24d77a11b6
Jennifer <jenndestes@gmail.com>
2021-05-26 18:03:19 -0500

    Added foreign key by reference

--- backend/db/migrate/20210525012124_add_user_id_to_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToExpenses < ActiveRecord::Migration[6.1]
-  def change
-    add_column :expenses, :user_id, :integer
-  end
-end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_012124) do
+ActiveRecord::Schema.define(version: 2021_05_26_230146) do
 
   create_table "expenses", force: :cascade do |t|
     t.date "date"
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 2021_05_25_012124) do
     t.decimal "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_05_25_012124) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "expenses", "users"
 end
