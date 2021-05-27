0b9810dfa90202a19f160777feeb6cb6a5268ceb
Jennifer <jenndestes@gmail.com>
2021-05-26 23:22:10 -0500

    Added button to edit each expense

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,18 @@ function fetchExpenses() {
     const expensesContainer = document.getElementById("expenses-container")
     // returns promise
     fetch("http://localhost:3000/api/v1/expenses")
-        // Async too, returns promise
+        // capturing response, parsing it into json
         .then(resp => resp.json())
         .then(data => {
             data.forEach(expense => {
                 expensesContainer.innerHTML += `<li>${expense.date}</li>`
                 expensesContainer.innerHTML += `<li>${expense.description}</li>`
-                expensesContainer.innerHTML += `<li>${expense.amount}</li>`
+                expensesContainer.innerHTML += `<li>${expense.amount}<br><button data-id=${expense.id}>Edit</button><br><br></li>`
             })
         })
     // .catch(err => console.warn(err))
+
 }
+
+// Sign up function
+// Sign in function
