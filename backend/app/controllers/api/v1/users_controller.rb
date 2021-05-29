b72ce1ea4483efeb9cd218db7d9fea130b6bcdb1
Jennifer <jenndestes@gmail.com>
2021-05-28 23:45:04 -0500

    Commented out methods i'm not yet using

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  # before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
   # def index
@@ -12,9 +12,9 @@ class Api::V1::UsersController < ApplicationController
   # end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
@@ -27,19 +27,20 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
   # Use callbacks to share common setup or constraints between actions.
@@ -50,5 +51,9 @@ class Api::V1::UsersController < ApplicationController
   # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:email, :password_digest, :amount, :Expense)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password_digest)
   end
 end
