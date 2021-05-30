a38176b0d4826537a00bea89d26aa853227dabd1
Jennifer <jenndestes@gmail.com>
2021-05-30 17:34:01 -0500

    Commenting out session controller because I do not think I'll have enough time to do signin/signup functionality

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
 
-  def new
-    if logged_in?
-    end
-  end
-
-  def create
-    user = User.find_by(:email => params[:email])
-    if user && user.authenticate(params[:password])
-      render :json => user, :only => [:id, :email]
-    else
-      render :json => { errors: "Your login information does not match our records. Please try again." }
-    end
-  end
-
-  def destroy
-    session.clear
-  end
+  # def new
+  #   if logged_in?
+  #   end
+  # end
+  #
+  # def create
+  #   user = User.find_by(:email => params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     render :json => user, :only => [:id, :email]
+  #   else
+  #     render :json => { errors: "Your login information does not match our records. Please try again." }
+  #   end
+  # end
+  #
+  # def destroy
+  #   session.clear
+  # end
 
 end
