6398f57ca8a24152084e010f85b74591f9f53ba4
Jennifer <jenndestes@gmail.com>
2021-05-30 22:32:19 -0500

    Created new class to encapsulate form methods and variables

---
+++ frontend/src/createExpenseForm.js
@@ -0,0 +1,15 @@
+class CreateExpenseForm {
+    constructor() {
+        this.user = parseInt(document.querySelector("#users").value)
+        this.date = document.querySelector("#date").value
+        this.description = document.querySelector("#description").value
+        this.amount = document.querySelector("#amount").value
+    }
+
+    static createFormHandler(e) {
+        e.preventDefault()
+        console.log(e)
+        const formData = new CreateExpenseForm()
+        fetchExpensesPost(formData)
+    }
+}
