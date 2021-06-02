d10338a3d44eb7f3858b6fe03c925a19d1b3e3f8
Jennifer <jenndestes@gmail.com>
2021-06-02 14:45:46 -0500

    Commented out unused methods

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -17,15 +17,15 @@ class Api::V1::UsersController < ApplicationController
   # end
 
   # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #
+  #   if @user.save
+  #     render json: @user, status: :created, location: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
 
   # PATCH/PUT /users/1
@@ -42,13 +42,13 @@ class Api::V1::UsersController < ApplicationController
   #   @user.destroy
   # end
 
-  private
+  # private
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:email, :password_digest)
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
+  #
+  # def user_params
+  #   params.require(:user).permit(:email)
+  # end
 end
