4654955cce8d547edee18878439a2cc3adaad203
Jennifer <jenndestes@gmail.com>
2021-06-02 20:36:06 -0500

    Removed password_digest from database because I am no longer using authentication

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_230146) do
+ActiveRecord::Schema.define(version: 2021_06_02_194825) do
 
   create_table "expenses", force: :cascade do |t|
     t.date "date"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2021_05_26_230146) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
