37137834ff3b6fa0b4583e8845cce6b65736d483
Jennifer <jenndestes@gmail.com>
2021-06-04 15:00:01 -0500

    Changed name of class because it previously was the same name as a variable in index.js

---
+++ frontend/src/expenseForm.js
@@ -0,0 +1,15 @@
+class ExpenseForm {
+    constructor() {
+        this.user_id = parseInt(document.querySelector('#users').value)
+        this.date = document.querySelector('#date').value
+        this.description = document.querySelector('#description').value
+        this.amount = document.querySelector('#amount').value
+    }
+
+    static createFormHandler(e) {
+        e.preventDefault()
+        console.log(e)
+        const formData = new ExpenseForm()
+        fetchExpensesPost(formData)
+    }
+}
