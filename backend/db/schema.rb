7923f4c70b31cfa820c3177efbe5ef4226242316
Jennifer <jenndestes@gmail.com>
2021-06-04 15:07:16 -0500

    Added foreign key to database because it somehow previously got deleted

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_194825) do
+ActiveRecord::Schema.define(version: 2021_06_04_042831) do
 
   create_table "expenses", force: :cascade do |t|
     t.date "date"
